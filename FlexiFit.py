2560ad554f745bfc48b625da8ab62a6df17303a2
dragosplesa <plesadragos9@gmail.com>
2024-01-10 16:46:23 +0200

    added Login functionality

--- FlexiFit.py
+++ FlexiFit.py
@@ -1,5 +1,65 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
+
+def center_window(width=300, height=200, window=None):
+    # get screen width and height
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+
+    # calculate position x and y coordinates
+    x = (screen_width/2) - (width/2)
+    y = (screen_height/2) - (height/2)
+    window.geometry('%dx%d+%d+%d' % (width, height, x, y))
+
+class LoginClass:
+    def __init__(self, root, login_success):
+        self.root = root
+        self.root.title("Login Page")
+        self.login_success = login_success
+
+        # Variables to store login data
+        self.username_var = tk.StringVar()
+        self.password_var = tk.StringVar()
+
+        # Create GUI elements
+        self.create_widgets()
+
+    def create_widgets(self):
+        # Username Entry
+        username_label = ttk.Label(self.root, text="Username:")
+        username_label.pack(pady=10)
+
+        username_entry = ttk.Entry(self.root, textvariable=self.username_var)
+        username_entry.pack(pady=10)
+
+        # Password Entry
+        password_label = ttk.Label(self.root, text="Password:")
+        password_label.pack(pady=10)
+
+        password_entry = ttk.Entry(self.root, textvariable=self.password_var)
+        password_entry.pack(pady=10)
+
+        # Login Button
+        login_button = ttk.Button(self.root, text="Login", command=self.login)
+        login_button.pack(pady=20)
+
+    def login(self):
+        username = self.username_var.get()
+        password = self.password_var.get()
+
+        if username == "user" and password == "user":
+            tk.messagebox.showinfo(title="Login Success", message="You have successfully logged in")
+            self.root.destroy()
+            self.login_success()
+        else:
+            tk.messagebox.showerror(title="Login Failed", message="Incorrect username or password")
+
+def start_gym_app(root):
+    new_window = tk.Tk()
+    # new_window.geometry("300x500")
+    center_window(350, 500, new_window)
+    gym_app = GymApp(new_window)
 
 class GymApp:
     def __init__(self, root):
@@ -69,6 +129,6 @@ class GymApp:
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.geometry("1080x960")
-    app = GymApp(root)
+    center_window(500, 400, root)
+    login = LoginClass(root, lambda: start_gym_app(root))
     root.mainloop()
